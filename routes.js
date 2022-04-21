11cf7e5248fbfa53a4aa212b0dfe3575f1121e60
Jack Baltzer <jabb@tec.dk>
2022-04-21 08:49:58 +0200

    CRUD done

--- routes.js
+++ routes.js
@@ -3,50 +3,12 @@ const Beer = require('./models/beerModel');
 module.exports = (app) => {
 
   app.get('/', async (req, res) => {
-
-
-
     let beers = await Beer.find();
     res.render('index', {
       beers,
       message: '<i>Beer</i>',
       data: [1, 2, 3, 4, 5, 6, 7]
     });
-  });
-
-  app.post('/', async (req, res) => {
-
-    let message = [];
-    if (req.body.beerName == undefined || req.body.beerName == '') {
-      message.push('Udfyld navn');
-    }
-    if (req.body.beerType == undefined || req.body.beerType == '') {
-      message.push('vælg øltype');
-    }
-    if (req.body.color == undefined || req.body.color == '') {
-      message.push('udflyd farve');
-    }
-
-    if (message.length > 0) {
-      // HER ER DER SKET FEJL
-      res.render('index', {
-        message: message.join(', '),
-        data: [1, 2, 3, 4, 5, 6, 7],
-        beer: req.body
-      });
-    } else {
-      // INDSÆT I DB
-      await Beer.create({
-        name: req.body.beerName,
-        color: req.body.color,
-        type: req.body.beerType
-      });
-      res.redirect('/');
-    }
-
-
-
-
   });
 
   app.get('/admin/beer', async (req, res) => {
@@ -70,29 +32,26 @@ module.exports = (app) => {
     }
 
     if (message.length > 0) {
+      let beers = await Beer.find();
       // HER ER DER SKET FEJL
-      res.render('index', {
+      res.render('beerAdmin', {
         message: message.join(', '),
-        data: [1, 2, 3, 4, 5, 6, 7],
-        beer: req.body
+        beer: req.body,
+        beers
       });
     } else {
       // INDSÆT I DB
-      await Beer.create({
-        name: req.body.name,
-        color: req.body.color,
-        type: req.body.type
-      });
+      await Beer.create(req.body);
       res.redirect('/admin/beer');
     }
   });
 
-  app.get('/admin/beer/edit/:beerId',  (req, res) => {
+  app.get('/admin/beer/edit/:beerId', (req, res) => {
     Beer.findById(req.params.beerId, async (err, beer) => {
-      if(err){
+      if (err) {
         console.log(err.message);
         res.redirect('/admin/beer');
-      }else{
+      } else {
         let beers = await Beer.find();
         res.render('beerAdmin', {
           beers,
@@ -103,4 +62,35 @@ module.exports = (app) => {
     });
   });
 
+  app.post('/admin/beer/edit/:beerId', async (req, res) => {
+    let message = [];
+    if (req.body.name == undefined || req.body.name == '') {
+      message.push('Udfyld navn');
+    }
+    if (req.body.type == undefined || req.body.type == '') {
+      message.push('vælg øltype');
+    }
+    if (req.body.color == undefined || req.body.color == '') {
+      message.push('udflyd farve');
+    }
+
+    if (message.length > 0) {
+      let beers = await Beer.find();
+      // HER ER DER SKET FEJL
+      res.render('beerAdmin', {
+        message: message.join(', '),
+        beer: req.body,
+        beers
+      });
+    } else {
+      // INDSÆT I DB
+      await Beer.findByIdAndUpdate(req.params.beerId, req.body);
+      res.redirect('/admin/beer');
+    }
+  });
+
+  app.get('/admin/beer/delete/:beerId', async (req, res) => {
+    await Beer.findByIdAndDelete(req.params.beerId);
+    res.redirect('/admin/beer');
+  });
 }
